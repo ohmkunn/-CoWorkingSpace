c68219c5fdfde1245378a50a2cde080b95c8f0bf
witsaroot jongsanguandee <pccphet77@gmail.com>
2023-11-17 12:41:05 +0700

    Add component userRegister.tsx

---
+++ frontend/src/libs/userRegister.tsx
@@ -0,0 +1,22 @@
+export default async function userRegister(userName:string, userEmail:string, userTel:string, userRole:string, userPassword:string) {
+    const response = await fetch("http://localhost:5000/api/v1/auth/register", {
+        method: "POST" ,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+            tel: userTel,
+            role: userRole,
+            password: userPassword,
+        }),
+    })
+    
+    if(!response.ok) {
+        throw new Error("Failed to register")
+    }
+        
+    return await response.json()
+}
+    
