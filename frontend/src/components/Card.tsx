f3465f0a2604b290f9e3fe4e874433f2db685bcb
ohm <ohmkunn@hotmail.com>
2023-11-17 19:45:14 +0700

    make reservation system for user

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,58 +1,19 @@
-import React, { useState,useEffect } from 'react';
+'use client'
+import React, { useState } from 'react';
 import Image from 'next/image';
-import InteractiveCard from './InteractiveCard';
-// import { Rating, Typography } from '@mui/material';
+import ReservationModal from './ReservationModal';
 
 export default function Card(
     { name
-        ,operatingHours,address,province,postalcode,tel, picture
-    }: { name:string
+        ,operatingHours,address,province,postalcode,tel, picture,token,sid
+    }: { name:string, token:string, sid:string
         ,operatingHours:string,address:string,province:string,postalcode:string,tel:string, picture:string
       }
     ) {
-    // const [ratingValue, setRatingValue] = useState<number>(0); // Initialize with your desired default value
-    
-    // const handleRatingChange = (newValue: number | null) => {
-    //     if (newValue !== null) {
-    //         setRatingValue(newValue);
-    //         if(onCompare){
-    //             onCompare(Name, newValue); // Pass both the Name and the new rating value to the parent
-    //         }
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     if(!Ratinglist.has(Name)){
-    //         setRatingValue(0); // Reset the rating to 0 when the component mounts or when the Name prop changes
-    //     }
-    // }, [Ratinglist]);
-
+        const [open, setOpen] = useState(false)
     return (
-        // <InteractiveCard>
-            // <div className='w-full h-[60%] relative rounded-t-lg'>
-            //     <Image
-            //         src={imgSrc}
-            //         alt='hospitalPicture'
-            //         fill={true}
-            //         objectFit='cover'
-            //         className='rounded-t-lg'
-            //     />
-            // </div>
-        //     <div className='w-full h-[25%] p-[10px]'>
-        //         <h3 className='font-semibold'>{Name}</h3>
-        //         <p>click for more detail.</p>
-        //     </div>
-        //     {/* <div className='px-4'>
-        //         <Typography component="legend">Rating</Typography>
-        //         <Rating
-        //             name="simple-controlled"
-        //             value={ratingValue}
-        //             precision={0.5}
-        //             onChange={(e, newValue) => {handleRatingChange(newValue)}}
-        //             onClick={(e) => {e.stopPropagation()}}
-        //         />
-        //     </div> */}
-        // </InteractiveCard>
+        <>
+        <ReservationModal sid={sid}token={token} open={open} setOpen={setOpen}/>
         <div className="bg-white rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 w-80">
       <div className="p-1 bg-sky-200">
       </div>
@@ -69,7 +30,8 @@ export default function Card(
                 />
         </div>
         
-        <p className="text-3xl font-bold text-gray-800 mb-6">{operatingHours}</p>
+        <p className="text-xl font-bold text-gray-800">Operating Hour</p>
+        <p className="text-lg font-medium text-gray-800 mb-6">{operatingHours}</p>
         <ul className="text-sm text-gray-600 mb-6">
           <li className="mb-1 flex items-center">
             
@@ -91,10 +53,12 @@ export default function Card(
       </div>
       <div className="p-4">
         <button
+            onClick={()=>setOpen(!open)}
           className="w-full bg-sky-600 text-white rounded-full px-4 py-2 hover:bg-sky-950 focus:outline-none focus:shadow-outline-purple active:bg-purple-800">
           Reservation
         </button>
       </div>
     </div>
+    </>
     );
 }
--- frontend/src/components/Panel.tsx
+++ frontend/src/components/Panel.tsx
@@ -1,54 +1,21 @@
-// 'use client'
 import React, { useReducer } from 'react';
 import Card from './Card';
 import Link from 'next/link';
 import getCoworkingspaces from '@/libs/getCoworkingspaces';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { getServerSession } from 'next-auth'
 
 export default async function Panel() {
 
-    // mockdata
-    // const mockHospital = [{hid:"001", name:'Chulalongkorn Hospital', image:'/chula.jpg'},
-    //     {hid:"002",name:'Rajavithi Hospital', image:'/rajavithi.jpg'},
-    //     {hid:"003",name:'Thammasat University Hospital' ,image:'/thammasat.jpg'}]
-
-    // const compareReducer = (ratingList: Map<string, number>, action: { type: string, Name: string, Rating?: number }) => {
-    //     switch (action.type) {
-    //         case 'add': {
-    //             const newRatingList = new Map(ratingList);
-    //             const ratingToAdd = action.Rating !== undefined ? action.Rating : 0; // Set the default rating to 0
-    //             newRatingList.set(action.Name, ratingToAdd);
-    //             return newRatingList;
-    //         }
-    //         case 'remove': {
-    //             const newRatingList = new Map(ratingList);
-    //             newRatingList.delete(action.Name);
-    //             return newRatingList;
-    //         }
-    //         default:
-    //             return ratingList;
-    //     }
-    // };
-    
-
-    // const [raitingList, dispatchCompare] = useReducer(compareReducer, new Map<string, number>());
-
-    // const handleDelete = (Name: string) => {
-    //     dispatchCompare({ type: 'remove', Name });
-    // };
-
-    // const handleRatingClick = (Name: string) => {
-    //     // Call the handleDelete function when a rating is clicked
-    //     handleDelete(Name);
-    // };
-
     const coworkingSpaces = await getCoworkingspaces()
-
+    const session = await getServerSession(authOptions)
+    
     return (
         
         <div className="bg-gray-100 min-h-screen py-2 flex items-center justify-center w-full p-0 flex-col">
             <h1 className='text-5xl font-extrabold text-gray-800 mb-6 text-left'>Browsing Your Space.</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {coworkingSpaces.data.map((spaceItem:Object)=>(
+        { (session?.user.token != null)?coworkingSpaces.data.map((spaceItem:Object)=>(
             <Card name={spaceItem.name}
                 address={spaceItem.address}
                 operatingHours={spaceItem.operatingHours}
@@ -57,8 +24,10 @@ export default async function Panel() {
                 province={spaceItem.province}
                 tel={spaceItem.tel}
                 key={spaceItem.name}
+                token={session?.user.token}
+                sid={spaceItem._id}
             />
-        ))}
+        )):null}
             {/* <Card name={"spaceItem.name"}/>
             <Card name={"spaceItem.name"}/>
             <Card name={"spaceItem.name"}/> */}
