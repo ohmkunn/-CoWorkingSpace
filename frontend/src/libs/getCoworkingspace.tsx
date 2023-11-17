2bf2853fa235fef5173bedc11f67cfffc329a3e9
witsaroot jongsanguandee <pccphet77@gmail.com>
2023-11-17 12:11:18 +0700

    Add component getCoworkspaces.tsx & getCoworkspace.tsx

---
+++ frontend/src/libs/getCoworkingspace.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default async function getCoworkingspaces(id:string) {
+    const response = await fetch(`http://localhost:5000/api/v1/coworkingspaces/${id}`)
+    if(!response.ok){
+        throw new Error("Failed to fetch coworkingspace")
+    } else {
+        console.log("correct")
+    }
+    return await response.json()
+}
---
+++ frontend/src/libs/getCoworkingspaces.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default async function getCoworkingspaces() {
+
+//add timeout for loading delay testing
+    await new Promise((resolve) => setTimeout(resolve,5000))
+
+    const response = await fetch("http://localhost:5000/api/v1/coworkingspaces", 
+        // {next: {tags:['hospitals']}}
+    )
+    if(!response.ok){
+        throw new Error("Failed to fetch coworkingspaces")
+    } 
+    return await response.json()
+}
